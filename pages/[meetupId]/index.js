026c1c5bb14b05337f207feddc015b7bcfe224bb
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-07-15 22:29:06 +0700

    Adding Custom Components & CSS Modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/1/1d/Brno_-_Veve%C5%99%C3%AD_-_Stavebn%C3%AD_fakulta_VUT.jpg"
+      title="First Meetup"
+      address="Some Stree 5, Some City"
+      description="The is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
